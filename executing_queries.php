fd32157b88d24a8037367798166e16d71af6de30
Vahabov <kamilvahabov5@gmail.com>
2021-04-18 21:20:06 -0700

    executing_queries.php

--- executing_queries.php
+++ executing_queries.php
@@ -1,111 +1,123 @@
-
 <!DOCTYPE html>
 <html lang="az">
+
 <head>
     <?php include("inc/head.php"); ?>
 </head>
+
 <body class="az">
 
 
 
-<?php
-include("inc/header.php")
-?>
-<section class="content ">
-    <section class="content">
-        <div class="container">
-            <div class="row">
-                <div class="page-head col-xs-12">
-                    <div class="col-md-3 col-sm-6 col-xs-6"><h4>İSTİFADƏÇİ <span>PANELİ</span></h4></div>
-                    <div class="col-md-9 col-sm-6 col-xs-6"><span class="user-code">MÜŞTƏRİ KODU:  <b>0000663</b></span>
-                        <ol class="breadcrumb web">
-                            <li><a href="/">Ana səhifə </a></li>
-                            <li class="active">İstifadəçi paneli</li>
-                        </ol>
+    <?php
+    include("inc/header.php")
+    ?>
+    <section class="content ">
+        <section class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="page-head col-xs-12">
+                        <div class="col-md-3 col-sm-6 col-xs-6">
+                            <h4>İSTİFADƏÇİ <span>PANELİ</span></h4>
+                        </div>
+                        <div class="col-md-9 col-sm-6 col-xs-6"><span class="user-code">MÜŞTƏRİ KODU: <b>0000663</b></span>
+                            <ol class="breadcrumb web">
+                                <li><a href="/">Ana səhifə </a></li>
+                                <li class="active">İstifadəçi paneli</li>
+                            </ol>
+                        </div>
                     </div>
-                </div>
-                <div id="sticky-anchor"></div>
-                <div class="panel-page col-xs-12">
-                    <div class="row relative">
-                        <?php include "inc/sidebar.php"; ?>
-                        <div class="col-md-9 col-sm-11 col-xs-11 coin" shop="0">
-                            <div class="row relative user_panel">
-                                <div class="nav-panel">
-                                    <ul class="nav navbar-nav navbar-left">
-                                        <li><a href="panel_queries.php">Gözləyən sorğu</a></li>
-                                        <li class="active"><a href="executing_queries.php">İcrada olan sorğu</a></li>
-                                        <li><a href="finished_queries.php">Tamamlanmış sorğu</a></li>
-                                    </ul>
-
-                                </div>
-
-                                <div class="query_head">
-                                    <div class="row">
-                                        <div class="col-md-6 col-xs-12 query_button">
-                                            <button class="btn btn-effect"><i class="glyphicon glyphicon-plus"></i>  Sorğu yarat</button>
+                    <div id="sticky-anchor"></div>
+                    <div class="panel-page col-xs-12">
+                        <div class="row relative">
+                            <?php include "inc/sidebar.php"; ?>
+                            <div class="col-md-9 col-sm-11 col-xs-11 coin" shop="0">
+                                <div class="row relative user_panel">
+                                    <div class="nav-panel">
+                                        <ul class="nav navbar-nav navbar-left">
+                                            <li><a href="panel_queries.php">Sürücü</a></li>
+                                            <li class="active"><a href="executing_queries.php">Fərdi</a></li>
+                                            <li><a href="finished_queries.php">Korporativ</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="white_cards_package">
+                                        <div class="card_item standart_card">
+                                            <div class="head">
+                                                <div class="type_col">
+                                                    <img src="img/x-icons/ailehuququ.svg" alt="#">
+                                                    <span>Ailə hüququ</span>
+                                                </div>
+                                                <div class="balance_col">
+                                                    <span class="balance">
+                                                        50
+                                                        <img src="img/manat.svg" class="injectable" alt="">
+                                                    </span>
+                                                    <span class="title">Bir illik qiyməti</span>
+                                                </div>
+                                            </div>
+                                            <div class="foot">
+                                                <div class="left">
+                                                    <span class="label_date">Son istifadə müddəti</span>
+                                                    <span class="date">21.10.2021</span>
+                                                    <div class="progress">
+                                                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="right">
+                                                    <a href="#" class="details_btn">
+                                                        Ətraflı
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class="col-md-4 col-xs-12 pull-right">
-                                            <input type="text" class="form-control" placeholder="Axtarış">
+                                        <div class="card_item gold_card">
+                                            <div class="head">
+                                                <div class="type_col">
+                                                    <img src="img/x-icons/emlakqeydiyyati.svg" alt="#">
+                                                    <span>Əmlak qeydiyyatı</span>
+                                                </div>
+                                                <div class="balance_col">
+                                                    <span class="balance">
+                                                        150
+                                                        <img src="img/manat.svg" class="injectable" alt="">
+                                                    </span>
+                                                    <span class="title">Bir illik qiyməti</span>
+                                                </div>
+                                            </div>
+                                            <div class="foot">
+                                                <div class="left">
+                                                    <span class="label_date">Son istifadə müddəti</span>
+                                                    <span class="date">21.10.2021</span>
+                                                    <div class="progress">
+                                                        <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="right">
+                                                    <a href="#" class="details_btn">
+                                                        Ətraflı
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="query_list col-md-12">
-                                    <table class="table table-striped table-queries">
-                                        <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Sorğu</th>
-                                            <th></th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Bir çox proqram araçları mövcud təsadüfi ...
-                                            <br />
-                                                <span>10.09.2020</span>
-                                            </td>
-                                            <td><img src="img/icons/orange-point.png"></td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Bir çox proqram araçları mövcud təsadüfi ...
-                                            <br />
-                                                <span>10.09.2020</span>
-                                            </td>
-                                            <td><img src="img/icons/orange-point.png"></td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Bir çox proqram araçları mövcud təsadüfi ...
-                                            <br />
-                                                <span>10.09.2020</span>
-                                            </td>
-                                            <td><img src="img/icons/orange-point.png"></td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-
-
                             </div>
-
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
     </section>
 
-</section>
+    <?php
+    include("inc/footer.php");
+    ?>
 
-<?php
-include ("inc/footer.php");
-?>
-
-<?php
-include ("inc/foot.php");
-?>
+    <?php
+    include("inc/foot.php");
+    ?>
 </body>
+
 </html>
